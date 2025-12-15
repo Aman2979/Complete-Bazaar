953df59ef4353a2bd635019d6a5e06d4dfe697be
Aman Kumar <ak93896897@gmail.com>
2025-12-15 19:12:17 +0530

    Done till cart

--- Backend/app.js
+++ Backend/app.js
@@ -10,15 +10,19 @@ const cors = require("cors");
 const sellerRouter = require("./routers/sellerRouter");
 const errorController = require("./controllers/errorController");
 const authRouter = require("./routers/authRouter");
+const { isLoggedIn, isSeller, isCustomer } = require("./middleware/auth");
+const customerRouter = require("./routers/customerRouter");
 
 const MONGO_DB_URL = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@aman0001.w1coczr.mongodb.net/${process.env.MONGO_DB_DATABASE}`;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use('/uploads', express.static('uploads'));
 app.use(express.json());
-app.use('/api/auth', authRouter);
-app.use("/api/seller", sellerRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/seller", isLoggedIn, isSeller, sellerRouter);
+app.use("/api/customer",isLoggedIn, isCustomer, customerRouter);
 app.use(errorController.get404);
 
 const PORT = process.env.PORT || 3000;
